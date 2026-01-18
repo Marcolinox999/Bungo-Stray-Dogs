2bcea9c21fdc3c8d2d928c43b59a44aae28dc081
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-01-18 22:53:10 +0100

    Finally BOSSS WORKSSSSS

--- Assets/Scripts/TriggerObject/Zone/SpawnerAttack.cs
+++ Assets/Scripts/TriggerObject/Zone/SpawnerAttack.cs
@@ -1,16 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class SpawnerAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject m_projectile;
+    [SerializeField] GameObject[] m_spawn;
+    [SerializeField] private float spawnTime;
+    private Random m_spawnPointRandom;
+    private bool m_spawned = false;
+    private IEnumerator SpawnProjectile()
     {
+        if (m_spawned == false)
+        {
+            m_spawned = true;
+            yield return new WaitForSeconds(spawnTime);
+            int selectedPointRandom = UnityEngine.Random.Range(0, m_spawn.Length);
+            Instantiate(m_projectile, m_spawn[selectedPointRandom].transform.position, Quaternion.identity);
+            m_spawned = false;
+        }
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        StartCoroutine(SpawnProjectile());
     }
 }

ac7c08899c6731cff38c9e427950eb28173ab373
Alejandro_Martin_del_Rey <alejandro.del@live.u-tad.com>
2026-01-18 20:49:50 +0100

    PoisonDamage

--- Assets/Scripts/TriggerObject/Zone/PoisonZone.cs
+++ Assets/Scripts/TriggerObject/Zone/PoisonZone.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PoisonZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] SpriteRenderer m_playerSpriteRenderer;
+    [SerializeField] CharacterBeatController m_player;
+    public int damage;
+    private bool isPoison = true;
+    public IEnumerator Poison(int poisonTimes, float poisonDelay)
     {
-        
+        if (isPoison)
+        {
+            m_playerSpriteRenderer.color = Color.gray;
+            Debug.Log("Poison damage");
+            isPoison = false;
+            for (int i = 0; i < poisonTimes; i++)
+            {
+                yield return new WaitForSeconds(poisonDelay);
+                m_player.AddHealth(damage);
+            }
+            isPoison = true;
+            m_playerSpriteRenderer.color = Color.white;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PoisonDamage(int times, float delay)
     {
-        
+        StartCoroutine(Poison(times, delay));
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerBeatController>() != null)
+        {
+            PoisonDamage(2,2f);
+        }
     }
 }
